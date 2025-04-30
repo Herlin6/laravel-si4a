bb9e84bfa5a6e26d7439a52c713c9cc39c188e3a
Herlin6 <156332335+Herlin6@users.noreply.github.com>
2025-04-30 14:44:09 +0700

    add: fakultas table

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\FakultasController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/profile', function () {
-    return view('profile');
+Route::get('/profil', function () {
+    return view('profil');
 });
+Route::resource('/fakultas', FakultasController::class); // resource untuk menghandle 7 fungsi yg ada di controller
