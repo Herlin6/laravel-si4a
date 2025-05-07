3b446bdd920ab69e52eeb8edabc8bf1c96b0c9a3
Herlin6 <156332335+Herlin6@users.noreply.github.com>
2025-05-07 14:58:40 +0700

    add: adminlte

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,31 +1,64 @@
-@extends('layout.main') @section('content')
-<h1>Mahasiswa</h1>
+@extends('layout.main') @section('content') @section('title', 'Mahasiswa')
+<div class="row">
+    <div class="col-12">
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">List Mahasiswa</h3>
+                <div class="card-tools">
+                    <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-collapse"
+                        title="Collapse"
+                    >
+                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-remove"
+                        title="Remove"
+                    >
+                        <i class="bi bi-x-lg"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-striped">
+                    <tr>
+                        <th>NPM</th>
+                        <th>Nama</th>
+                        <th>Jenis Kelamin</th>
+                        <th>Tanggal Lahir</th>
+                        <th>Tempat Lahir</th>
+                        <th>Asal SMA</th>
+                        <th>Program Studi</th>
+                    </tr>
+                    @foreach ($mahasiswa as $item)
+                    <tr>
+                        <td>{{ $item->npm }}</td>
+                        <td>{{ $item->nama }}</td>
+                        <td>
+                            @php if ($item->jenis_kelamin == 'L') { echo
+                            'Laki-laki'; } else { echo 'Perempuan'; } @endphp
+                        </td>
+                        <td>{{ $item->tanggal_lahir }}</td>
+                        <td>{{ $item->tempat_lahir }}</td>
+                        <td>{{ $item->asal_sma }}</td>
+                        <td>{{ $item->prodi->nama }}</td>
+                    </tr>
 
-<table>
-    <tr>
-        <th>NPM</th>
-        <th>Nama</th>
-        <th>Jenis Kelamin</th>
-        <th>Tanggal Lahir</th>
-        <th>Tempat Lahir</th>
-        <th>Asal SMA</th>
-        <th>Program Studi</th>
-    </tr>
-    @foreach ($mahasiswa as $item)
-    <tr>
-        <td>{{ $item->npm }}</td>
-        <td>{{ $item->nama }}</td>
-        <td>
-            @php if ($item->jenis_kelamin == 'L') { echo 'Laki-laki'; } else {
-            echo 'Perempuan'; } @endphp
-        </td>
-        <td>{{ $item->tanggal_lahir }}</td>
-        <td>{{ $item->tempat_lahir }}</td>
-        <td>{{ $item->asal_sma }}</td>
-        <td>{{ $item->prodi->nama }}</td>
-    </tr>
-
-    @endforeach
-</table>
+                    @endforeach
+                </table>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">Footer</div>
+            <!-- /.card-footer-->
+        </div>
+        <!-- /.card -->
+    </div>
+</div>
 
 @endsection
--- resources/views/prodi/index.blade.php
+++ resources/views/prodi/index.blade.php
@@ -1,24 +1,58 @@
-@extends('layout.main') @section('content')
-<h1>Program Studi</h1>
+@extends('layout.main') @section('content') @section('title', 'Prodi')
+<div class="row">
+    <div class="col-12">
+        <!-- Default box -->
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">List Prodi</h3>
+                <div class="card-tools">
+                    <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-collapse"
+                        title="Collapse"
+                    >
+                        <i data-lte-icon="expand" class="bi bi-plus-lg"></i>
+                        <i data-lte-icon="collapse" class="bi bi-dash-lg"></i>
+                    </button>
+                    <button
+                        type="button"
+                        class="btn btn-tool"
+                        data-lte-toggle="card-remove"
+                        title="Remove"
+                    >
+                        <i class="bi bi-x-lg"></i>
+                    </button>
+                </div>
+            </div>
+            <div class="card-body">
+                <table class="table table-bordered table-striped">
+                    <tr>
+                        <th>Nama</th>
+                        <th>Singkatan</th>
+                        <th>kaprodi</th>
+                        <th>Sekretaris</th>
+                        <th>Fakultas</th>
+                    </tr>
+                    @foreach ($prodi as $item)
+                    <tr>
+                        <td>{{ $item->nama }}</td>
+                        <td>{{ $item->singkatan }}</td>
+                        <td>{{ $item->kaprodi }}</td>
+                        <td>{{ $item->sekretaris }}</td>
+                        <td>{{ $item->fakultas->nama }}</td>
+                        {{-- fakultas: nama function di models// fakultas->dekan --}}
+                    </tr>
+                
+                    @endforeach
+                </table>
+            </div>
+            <!-- /.card-body -->
+            <div class="card-footer">Footer</div>
+            <!-- /.card-footer-->
+        </div>
+        <!-- /.card -->
+    </div>
+</div>
 
-<table>
-    <tr>
-        <th>Nama</th>
-        <th>Singkatan</th>
-        <th>kaprodi</th>
-        <th>Sekretaris</th>
-        <th>Fakultas</th>
-    </tr>
-    @foreach ($prodi as $item)
-    <tr>
-        <td>{{ $item->nama }}</td>
-        <td>{{ $item->singkatan }}</td>
-        <td>{{ $item->kaprodi }}</td>
-        <td>{{ $item->sekretaris }}</td>
-        <td>{{ $item->fakultas->nama }}</td>
-        {{-- fakultas: nama function di models// fakultas->dekan --}}
-    </tr>
-
-    @endforeach
-</table>
 @endsection
